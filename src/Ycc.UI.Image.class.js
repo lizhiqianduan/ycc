88e9ba7848551dfa285f42ea9c77ce44c5d639b4
xiaohei <1024669228@qq.com>
2017-11-22 16:30:59 +0800

    九宫格1 3 7 9位置

--- src/Ycc.UI.Image.class.js
+++ src/Ycc.UI.Image.class.js
@@ -19,7 +19,9 @@
 	 * 		<br> repeat -- 重复。左上角对齐，重复平铺图片，不修改rect大小，超出隐藏。
 	 * 		<br> scale -- 缩放。左上角对齐，缩放至整个rect区域，不修改rect大小。
 	 * 		<br> auto -- 自动。左上角对齐，rect大小自动适配图片。若图片超出rect，会动态修改rect大小。
+	 * 		<br> scale9Grid -- 9宫格模式填充。左上角对齐，中间区域将拉伸，不允许图片超出rect区域大小，不会修改rect大小。
 	 * @param option.res	{Image}	需要填充的图片资源。注：必须已加载完成。
+	 * @param option.scale9GridRect	{Ycc.Math.Rect}	9宫格相对于res图片的中间区域，当且仅当fillMode为scale9Grid有效。
 	 * @constructor
 	 * @extends Ycc.UI.Base
 	 */
@@ -33,7 +35,8 @@
 			rect:null,
 			rectBgColor:"transparent",
 			fillMode:"none",
-			res:null
+			res:null,
+			scale9GridRect:null
 		},option);
 	};
 	Ycc.UI.Image.prototype = new Ycc.UI.Base();
@@ -84,6 +87,50 @@
 						xRest,yRest);
 				}
 			}
+		}else if(this.option.fillMode === "scale9Grid"){
+			var centerRect = this.option.scale9GridRect;
+			var src,dest;
+			
+			// 第1块
+			src = new Ycc.Math.Rect(0,0,centerRect.x,centerRect.y);
+			dest = new Ycc.Math.Rect(rect.x,rect.y,centerRect.x,centerRect.y);
+			this.ctx.drawImage(this.option.res,
+				// 源
+				src.x,src.y,src.width,src.height,
+				// 目标
+				dest.x,dest.y,dest.width,dest.height
+			);
+
+			// 第3块
+			src = new Ycc.Math.Rect(centerRect.x+centerRect.width,0,img.width-centerRect.x-centerRect.width,centerRect.y);
+			dest = new Ycc.Math.Rect(rect.width-src.width+rect.x,rect.y,src.width,src.height);
+			this.ctx.drawImage(this.option.res,
+				// 源
+				src.x,src.y,src.width,src.height,
+				// 目标
+				dest.x,dest.y,dest.width,dest.height
+			);
+			
+			// 第7块
+			src = new Ycc.Math.Rect(0,centerRect.y+centerRect.height,centerRect.x,img.height-centerRect.y-centerRect.height);
+			dest = new Ycc.Math.Rect(rect.x,rect.y+rect.height-src.height,src.width,src.height);
+			this.ctx.drawImage(this.option.res,
+				// 源
+				src.x,src.y,src.width,src.height,
+				// 目标
+				dest.x,dest.y,dest.width,dest.height
+			);
+			
+			// 第9块
+			src = new Ycc.Math.Rect(centerRect.x+centerRect.width,centerRect.y+centerRect.height,img.width-centerRect.x-centerRect.width,img.height-centerRect.y-centerRect.height);
+			dest = new Ycc.Math.Rect(rect.width-src.width+rect.x,rect.y+rect.height-src.height,src.width,src.height);
+			this.ctx.drawImage(this.option.res,
+				// 源
+				src.x,src.y,src.width,src.height,
+				// 目标
+				dest.x,dest.y,dest.width,dest.height
+			);
+			
 		}
 		
 		
