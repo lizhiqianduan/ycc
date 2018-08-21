10b038c248332848c9a89b39276772dbcfaf9956
xiaohei <1024669228@qq.com>
2018-08-21 11:37:02 +0800

    图的广度优先搜索完成

--- src/Ycc.Graph.class.js
+++ src/Ycc.Graph.class.js
@@ -80,36 +80,15 @@
 	 * @return {boolean}
 	 */
 	Ycc.Graph.prototype.bfs = function (vArrId,cb,vSearchedId) {
-		if(this.type===1)
-			return this.bfsDirectedGraph(vArrId,cb,vSearchedId);
-		else if(this.type===2)
-			return this.bfsDirectedGraph(vArrId,cb,vSearchedId);
-		
-	};
-	
-	
-	
-	
-	
-	/**
-	 * 有向图的广度优先搜索
-	 * @param vArrId 顶点$id数组，代表从哪些顶点开始遍历
-	 * @param cb 回调函数
-	 *   若回调函数返回true，则遍历结束
-	 * @param [vSearchedId] 已遍历的$id数组
-	 *
-	 * @return {boolean}
-	 */
-	Ycc.Graph.prototype.bfsDirectedGraph = function (vArrId,cb,vSearchedId) {
 		vArrId = vArrId||[];
 		vSearchedId = vSearchedId || [];
 		
 		// 递归结束条件
+		if(vSearchedId.length===this.vList.length)
+			return true;
+		
+		// 若未结束，但长度为0，说明图中存在孤立部分，任取一个孤立部分的顶点，继续遍历
 		if(vArrId.length===0){
-			if(vSearchedId.length===this.vList.length)
-				return true;
-			
-			// 已遍历的个数与图顶点的总个数不等，说明该图存在孤立部分，任取一个孤立部分的顶点，继续遍历
 			var tempID = null;
 			for(var j=0;j<this.vList.length;j++){
 				if(vSearchedId.indexOf(this.vList[j].$id)===-1){
@@ -132,7 +111,7 @@
 		var next = [];
 		for(var k=0;k<vArrId.length;k++){
 			v = vMap[vArrId[k]];
-			var temp =  v.getToIDs().filter(function (id) {
+			var temp =  v.getAccessibleIds().filter(function (id) {
 				return vSearchedId.indexOf(id)===-1;
 			});
 			next = next.concat(temp);
@@ -140,6 +119,8 @@
 		
 		return this.bfs(next,cb,vSearchedId);
 	};
+	
+	
 	
 	
 	/**
@@ -180,45 +161,7 @@
 	};
 	
 	
-	/**
-	 * @todo
-	 * 创建一个无向图
-	 * @static
-	 * @param vArr {[{id,data,...}]} 顶点列表
-	 * @param eArr {[{fromId,toId,data,...}]}	边列表
-	 */
-	Ycc.Graph.createDirectedGraph  = function (vArr,eArr) {
-		var graph = new Ycc.Graph(1);
-		var vList = graph.vList;
-		var eList = graph.eList;
-		
-		vArr.forEach(function (v) {
-			vList.push(new Ycc.Graph.DirectedV(v));
-		});
-		
-		eArr.forEach(function (e) {
-			var from = null,to=null;
-			var edge = new Ycc.Graph.DirectedE();
-			for(var i =0;i<vList.length;i++){
-				// 两个都找到了就跳出去
-				if(from && to) break;
-				var v = vList[i];
-				if(v.data.id === e.fromId){
-					from = v;
-					v.outIDs.push(edge.$id);
-				}
-				if(v.data.id === e.toId){
-					to = v;
-					v.inIDs.push(edge.$id);
-				}
-			}
-			edge.init(from.$id,to.$id,e);
-			eList.push(edge);
-		});
-		return graph;
-	};
-	
-	
+
 	
 	
 	
@@ -260,7 +203,7 @@
 	 * 获取有向图中，某个节点指向的节点ID列表
 	 * @return {Array}
 	 */
-	Ycc.Graph.DirectedV.prototype.getToIDs = function () {
+	Ycc.Graph.DirectedV.prototype.getAccessibleIds = function () {
 		var ids = [];
 		this.outIDs.forEach(function (id) {
 			ids.push(eMap[id].toID);
@@ -319,11 +262,59 @@
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * 创建一个无向图
+	 * @static
+	 * @param vArr {[{id,data,...}]} 顶点列表
+	 * @param eArr {[{ids,data,...}]}	边列表，ids为边关联的两个顶点id列表，长度为2
+	 */
+	Ycc.Graph.createUnDirectedGraph  = function (vArr,eArr) {
+		var graph = new Ycc.Graph(2);
+		var vList = graph.vList;
+		var eList = graph.eList;
+		
+		vArr.forEach(function (v) {
+			vList.push(new Ycc.Graph.UnDirectedV(v));
+		});
+		
+		eArr.forEach(function (e) {
+			// 边的节点id列表
+			var ids = [];
+			var edge = new Ycc.Graph.UnDirectedE();
+			for(var i =0;i<vList.length;i++){
+				// 遍历的顶点
+				var v = vList[i];
+				// 两个都找到了就跳出去
+				if(ids.length===2) break;
+
+				if(v.data.id === e.ids[0] || v.data.id === e.ids[1]){
+					ids.push(v.$id);
+					v.eIDs.push(edge.$id);
+				}
+			}
+			
+			edge.init(ids,e);
+			eList.push(edge);
+		});
+		return graph;
+	};
+	
+	
+	
 	/**
 	 * 无向图中的顶点类
+	 * @param data {any} 顶点携带的数据
 	 * @constructor
 	 */
-	Ycc.Graph.UnDirectedV = function () {
+	Ycc.Graph.UnDirectedV = function (data) {
 		/**
 		 * 节点id
 		 * @type {number}
@@ -334,13 +325,39 @@
 		 * 节点所携带的数据
 		 * @type {any}
 		 */
-		this.data = null;
+		this.data = data;
 		
 		/**
-		 * 节点的边列表
-		 * @type {Ycc.Graph.UnDirectedE[]}
+		 * 节点的边$id列表
+		 * @type {number[]}
 		 */
-		this.listE = [];
+		this.eIDs = [];
+		
+		
+		// 放入map，方便查找
+		vMap[this.$id] = this;
+		
+	};
+	
+	
+	/**
+	 * 获取节点的可达节点id列表
+	 * @return {number[]}
+	 */
+	Ycc.Graph.UnDirectedV.prototype.getAccessibleIds = function () {
+		var accessibleIds=[];
+		for(var i=0;i<this.eIDs.length;i++){
+			// 边
+			var edge = eMap[this.eIDs[i]];
+			if(edge.vIDs[0]===this.$id){
+				accessibleIds.push(edge.vIDs[1]);
+			}
+			
+			if(edge.vIDs[1]===this.$id){
+				accessibleIds.push(edge.vIDs[0]);
+			}
+		}
+		return accessibleIds;
 	};
 	
 
@@ -364,11 +381,23 @@
 		this.data = null;
 		
 		/**
-		 * 关联的两个顶点数组
-		 * @type {Ycc.Graph.V[]}
+		 * 关联的两个顶点$id数组 长度为2
+		 * @type {number[]}
 		 */
-		this.listV = [];
+		this.vIDs = [];
 		
+		// 放入map，方便查找
+		eMap[this.$id] = this;
+	};
+	
+	/**
+	 * 无向图中的边类初始化
+	 * @param ids
+	 * @param data
+	 */
+	Ycc.Graph.UnDirectedE.prototype.init = function (ids, data) {
+		this.vIDs = ids;
+		this.data = data;
 	};
 	
 	
