32f64d41d3a14d328b0e38bcb76e4493f15018b7
xiaohei <1024669228@qq.com>
2018-08-30 11:32:22 +0800

    加载器去除冗余

--- src/Ycc.Loader.class.js
+++ src/Ycc.Loader.class.js
@@ -16,74 +16,6 @@
 	Ycc.Loader = function () {
 		this.yccClass = Ycc.Loader;
 		
-		/**
-		 * 保存加载之后的所有图片资源。键为资源名称；值为Image元素
-		 * @type {{}}
-		 */
-		this.imageRes = {};
-		
-		/**
-		 * 是否缓存图片。
-		 * imageRes中key相同时，不会重复加载。
-		 * @type {boolean}
-		 */
-		this.cache = true;
-	};
-	
-	
-	/**
-	 * 加载单个图片资源
-	 * @param imageSrc	{String}	图片的路径
-	 * @param endCb		{Function}	加载成功的回调
-	 * @private
-	 */
-	Ycc.Loader.prototype._loadImage = function (imageSrc,endCb) {
-		var img = new Image();
-		img.src = imageSrc;
-		img.onload = function () {
-			Ycc.utils.isFn(endCb) && endCb(img);
-		}
-	};
-	
-	/**
-	 * 加载图片资源列表
-	 * @param imagesSrc		{Object}	资源列表。键为资源名称；值为资源路径
-	 * @param progressCb	{function}	加载进度的回调，每加载成功一个资源都会调用一次
-	 * @param endCb			{function}	全部加载完成的回调
-	 */
-	Ycc.Loader.prototype.loadImageList = function (imagesSrc,endCb,progressCb) {
-		var self = this;
-		// 已加载图片的个数
-		var loadedNum = 0;
-		// 资源的名称
-		var keys = Object.keys(imagesSrc);
-		for(var i =0;i<keys.length;i++){
-			var src = imagesSrc[keys[i]];
-			if(self.cache){
-				var img = self.imageRes[keys[i]];
-				if(img){
-					imageOnload(img);
-					continue;
-				}
-			}
-			
-			
-			this._loadImage(src,(function (key) {
-				return function (img) {
-					self.imageRes[key] = img;
-					imageOnload(img);
-				}
-				
-			})(keys[i]));
-		}
-		
-		function imageOnload(img) {
-			loadedNum++;
-			Ycc.utils.isFn(progressCb)&&progressCb(img);
-			if(loadedNum===keys.length){
-				Ycc.utils.isFn(endCb)&&endCb(self.imageRes);
-			}
-		}
 	};
 	
 	/**
@@ -101,10 +33,7 @@
 	Ycc.Loader.prototype.loadResParallel = function (resArr, endCb, progressCb,endResArr,endResMap) {
 		endResArr = endResArr || [];
 		endResMap = endResMap || {};
-		if(resArr.length===endResArr.length){
-			endCb(endResArr,endResMap);
-			return;
-		}
+		
 		for(var i=0;i<resArr.length;i++){
 			var curRes = resArr[i];
 			var successEvent = "load";
@@ -121,21 +50,21 @@
 				curRes.res.src = curRes.url;
 				curRes.res.preload = "load";
 			}
-			curRes.res.addEventListener(successEvent,(function (curRes,index) {
-				return function () {
-					endResArr.push(curRes);
-					if(typeof curRes.name!=='undefined') endResMap[curRes.name] = curRes.res;
-					Ycc.utils.isFn(progressCb) && progressCb(curRes,true,index);
-				};
-			})(curRes,i));
-			curRes.res.addEventListener(errorEvent,(function (curRes,index) {
-				return function () {
-					endResArr.push(curRes);
-					if(typeof curRes.name!=='undefined') endResMap[curRes.name] = curRes.res;
-					Ycc.utils.isFn(progressCb) && progressCb(curRes,false,index);
-				};
-			})(curRes,i));
 			
+			curRes.res.addEventListener(successEvent,listener(curRes,i,true));
+			curRes.res.addEventListener(errorEvent,listener(curRes,i,false));
+			
+			
+			function listener(curRes,index,error) {
+				return function () {
+					endResArr.push(curRes);
+					if(typeof curRes.name!=='undefined') endResMap[curRes.name] = curRes.res;
+					Ycc.utils.isFn(progressCb) && progressCb(curRes,error,index);
+					if(resArr.length===endResArr.length){
+						endCb(endResArr,endResMap);
+					}
+				};
+			}
 		}
 	};
 	
@@ -194,18 +123,6 @@
 		});
 
 		
-	};
-	
-	
-	
-	
-	
-	/**
-	 * 是否使用缓存
-	 * @param b {boolean}
-	 */
-	Ycc.Loader.prototype.useCache = function (b) {
-		this.cache = b;
 	};
 	
 	/**
