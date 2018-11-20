b0d52d6eaaf1fab8da72464311e657ff5a22a2bf
xiaohei <1024669228@qq.com>
2018-11-20 16:52:38 +0800

    moveToucheEventList的bug

--- src/Ycc.polyfill.wx.js
+++ src/Ycc.polyfill.wx.js
@@ -8,14 +8,14 @@
 
 // 兼容微信
 if("undefined"!== typeof wx){
-	exports.Ycc = Ycc;
+	module.exports = Ycc;
 	// Ycc.prototype.getStageWidth = function () {
 	//
 	// };
 	
-	Ycc.Gesture.prototype._init = function () {
+	/*Ycc.Gesture.prototype._init = function () {
 		console.log('wx gesture init');
-	};
+	};*/
 	
 	Ycc.utils.isMobile = function () {
 		console.log('wx isMobile');
