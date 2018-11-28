03547c6e19accbf6167f7ffa689e05efc670906c
xiaohei <1024669228@qq.com>
2018-11-28 11:07:21 +0800

    gruntjs

--- GruntFile.js
+++ GruntFile.js
@@ -45,6 +45,9 @@ module.exports = function(grunt){
 					'src/Ycc.UI.Rect.class.js',
 					'src/Ycc.UI.CropRect.class.js',
 					'src/Ycc.UI.SingleLineText.class.js',
+					
+					// 组件
+					'src/Ycc.UI.ComponentButton.class.js',
 			
 					// polyfill
 					'src/Ycc.polyfill.wx.js',
