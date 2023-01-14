a9cd9f4e630bcfc058f0555ab5274b325503ae79
xiaohei <1024669228@qq.com>
2023-01-14 13:54:48 +0800

    ts版本初始化

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+const { ESLint } = require("eslint")
+
+
+/**
+ * @type {ESLint.ConfigData}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  rules: {
+    // quotes: ['error', 'single']
+    // 强制两个空格缩进
+    indent: ['error', 2],
+
+    // 允许函数不带返回值，默认返回void
+    '@typescript-eslint/explicit-function-return-type':0,
+
+    // 允许this的别名，即 let self=this这种写法
+    "@typescript-eslint/no-this-alias": 0
+  }
+}
